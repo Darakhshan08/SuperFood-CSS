b8a154e6f8d591e0894ac820646975007b8515c0
Darakhshan08 <darakhshanshah8@gmail.com>
2024-12-16 18:34:50 +0500

    initial commit 2

--- src/components/FeaturesTwo.tsx
+++ src/components/FeaturesTwo.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Image from 'next/image';
-import '../styles/FeaturesTwo.css';
+import '@/styles/FeaturesTwo.css';
 
 function FeaturesTwo() {
     const features = [
